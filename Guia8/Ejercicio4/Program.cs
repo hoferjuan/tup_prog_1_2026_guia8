3b5e68acbae8d984e7fdd126d8918c4cabb04b45
unknown <juanfrahofer@gmail.com>
2026-05-31 12:05:38 -0300

    Actualizando Guia 8

---
+++ Guia8/Ejercicio4/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Ejercicio4
+{
+    internal class Program
+    {
+        //Variables
+        static string jugador1, jugador2;
+        static int setGanados1, setGanados2;
+
+        #region Lógica
+        static void RegistrarJugadores(string nombre1, string nombre2)
+        {
+            jugador1 = nombre1;
+            jugador2 = nombre2;
+        }
+        static void RegistrarResultados(int resultado1, int resultado2)
+        {
+            if (resultado1 > resultado2)
+            {
+                setGanados1++;
+            }
+            else
+            {
+                setGanados2++;
+            }
+
+        }
+        static string DeterminarGanador()
+        {
+            if (setGanados1 > setGanados2)
+            {
+                return "El Ganador es :" + jugador1;
+            }
+            else
+            {
+                return "El Ganador es :" + jugador2;
+            }
+        }
+        #endregion
+
+        //Mostrar Pantallas
+        #region MostrarPantallas
+        static void MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.WriteLine("1- Registrar los nombres de los dos jugadores.\r\n2- Registrar los resultados de cada set de los jugadores.\r\n3- Mostrar el ganador.\r\n");
+        }
+        static void MostrarPantallaSolicitarNombreJugadores()
+        {
+            Console.WriteLine("Ingrese nombre del Jugador N°:1");
+            string nombre1 = Console.ReadLine();
+            Console.WriteLine("Ingrese nombre del Jugador N°:2");
+            string nombre2 = Console.ReadLine();
+            RegistrarJugadores(nombre1, nombre2);
+        }
+        static void MostrarPantallaSolicitarResultadoSet()
+        {
+            //SET1
+            Console.WriteLine("Ingrese Puntos SET 1 jugador N°1: ");
+            int resultado1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese Puntos SET 1 jugador N°2: ");
+            int resultado2 = Convert.ToInt32(Console.ReadLine());
+            RegistrarResultados(resultado1, resultado2);
+
+            //SET2
+            Console.WriteLine("Ingrese Puntos SET 2 jugador N°1: ");
+            resultado1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese Puntos SET 2 jugador N°2: ");
+            resultado2 = Convert.ToInt32(Console.ReadLine());
+            RegistrarResultados(resultado1, resultado2);
+
+            //SET3
+            Console.WriteLine("Ingrese Puntos SET 3 jugador N°1: ");
+            resultado1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese Puntos SET 3 jugador N°2: ");
+            resultado2 = Convert.ToInt32(Console.ReadLine());
+            RegistrarResultados(resultado1, resultado2);
+
+
+        }
+        static void MostrarPantallaGanador()
+        {
+            Console.WriteLine($"El ganador del partido es: {DeterminarGanador()}");
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            MostrarPantallaSolicitarOpcionMenu();
+            
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    MostrarPantallaSolicitarNombreJugadores();
+                    break;
+                case 2:
+                    MostrarPantallaSolicitarResultadoSet();
+                    break;
+                case 3:
+                    MostrarPantallaGanador();
+                    break;
+            }
+        }
+    }
+}
---
+++ Guia8/Ejercicio5/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    internal class Program
+    {
+        #region Lógica
+        static int DeterminarLosDiasDelMes(int mes, int anio)
+        {
+            int dias = 0;
+            if (mes < 7)
+            {
+                if (mes == 2)
+                {
+                    if (DeterminarSiEsBisiesto(anio))
+                    {
+                        dias = 29;
+                    }
+                    else
+                    {
+                        dias = 28;
+                    }
+                }
+                else
+                {
+                    if (mes % 2 != 0)
+                    {
+                        dias = 31;
+                    }
+                    else
+                    {
+                        dias = 30;
+                    }
+                }
+            }
+            else
+            {
+                if (mes >= 7)
+                {
+                    if (mes % 2 != 0)
+                    {
+                        dias = 30;
+                    }
+                    else
+                    {
+                        dias = 31;
+                    }
+                }
+            }
+            return dias;
+        }
+        static bool DeterminarSiEsBisiesto(int anio)
+        {
+            if (anio % 400 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                if (anio % 100 == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (anio % 4 == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        #endregion
+        //Mostrar Pantallas
+        #region MostrarPantallas
+        static void MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.WriteLine("1- Determinar Cantidad de Días del mes.\r\n2- Verificar Si el año es bisiesto.\r\n");
+        }
+        static void MostrarPantallaSolicitarMesAñoYDeterminarDias()
+        {
+            Console.WriteLine("Ingrese año");
+            int anio = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese mes");
+            int mes = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" El mes tiene " + DeterminarLosDiasDelMes(mes, anio) + " Días ");
+
+        }
+        static void MostrarPantallaSolicitarSiElAñoEsBisiesto()
+        {
+            Console.WriteLine("Ingrese el año");
+            int anio = Convert.ToInt32(Console.ReadLine());
+            if (DeterminarSiEsBisiesto(anio))
+            {
+                Console.WriteLine("El año es bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("El año no es bisiesto");
+            }
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+
+            MostrarPantallaSolicitarOpcionMenu();
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    MostrarPantallaSolicitarMesAñoYDeterminarDias();
+                    break;
+                case 2:
+                    MostrarPantallaSolicitarSiElAñoEsBisiesto();
+                    break;
+            }
+        }
+    }
+}
